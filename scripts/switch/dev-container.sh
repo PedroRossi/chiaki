c524df4bbdbf8bfabcf46ee2aa192e99ba1d0d58
Florian Märkl <info@florianmaerkl.de>
2026-01-02 19:35:03 +0100

    Switch build container updates
    * Build on v4 image
    * Enable bash_history and nxlink in dev-container.sh

--- scripts/switch/dev-container.sh
+++ scripts/switch/dev-container.sh
@@ -2,9 +2,15 @@
 
 cd "`dirname $(readlink -f ${0})`/../.."
 
+touch "`pwd`/scripts/switch/dev-container.bash_history"
+
+# port 28771 for nxlink stdio
 podman run --rm \
 	-v "`pwd`:/build/chiaki" \
 	-w "/build/chiaki" \
+	-v "`pwd`/scripts/switch/dev-container.bash_history:/root/.bash_history" \
+	-p 0.0.0.0:28771:28771 \
+	--name chiaki-switch-dev \
 	-it \
-	quay.io/thestr4ng3r/chiaki-build-switch:v3 \
+	quay.io/thestr4ng3r/chiaki-build-switch:latest \
 	/bin/bash
--- scripts/switch/run-podman-build-chiaki.sh
+++ scripts/switch/run-podman-build-chiaki.sh
@@ -6,5 +6,5 @@ podman run --rm \
 	-v "`pwd`:/build/chiaki" \
 	-w "/build/chiaki" \
 	-it \
-	quay.io/thestr4ng3r/chiaki-build-switch:v3 \
+	quay.io/thestr4ng3r/chiaki-build-switch:v4 \
 	${1:-/bin/bash -c "scripts/switch/build.sh"}
